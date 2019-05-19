63dde8ca815d65f2f490e21f30fb61de0590c791
Claudia <claudia.lz@outlook.com>
2019-05-19 00:41:13 -0500

    Primer commit alfanumerico

---
+++ Analizar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Alfanumerico
+{
+    public class Analizar
+   {
+        public int Resultado(string cadena)
+        {
+            
+            char[] caracteres = cadena.ToCharArray();
+            int contador = 0;
+            int contadorDos = 0;
+
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                if (caracteres[i] > 'A' && caracteres[i] < 'Z' || caracteres[i] > 'a' && caracteres[i] < 'z')
+                    contador++;
+
+                if (caracteres[i] > '1' && caracteres[i] < '9')
+                    contadorDos++;
+            }
+
+            Console.WriteLine("Las palabras son: " + contador);
+            Console.WriteLine("Los numeros son: " + contadorDos);
+            Console.ReadKey();          
+
+            return 0;
+
+        }
+
+        
+
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alfanumerico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingresa el dato");
+            var dato = Console.ReadLine();
+
+            Console.WriteLine("El dato es: " + dato);
+
+            Analizar ana = new Analizar();
+            ana.Resultado(dato);
+
+
+        }     
+           
+    }
+}
